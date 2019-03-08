e7104df706be2c8847160a1dd7a297a8238246de
Daniel Cavalcanti <daniel.cavalcanti@outlook.com.br>
2019-03-08 11:49:01 -0300

    firebase e ionicpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,32 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage,
-    ListPage
+ 
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage,
-    ListPage
+
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AngularFireAuth
   ]
 })
 export class AppModule {}
